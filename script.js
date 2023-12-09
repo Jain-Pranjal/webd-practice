7116e81ce66a51f02f8a92c407a946b284e4b28f
Pranjal Jain <pranjalworkon@gmail.com>
2023-12-09 09:48:24 +0530

    intro to js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// basically we can perform the js excution at the server side by using the node js 
+
 // alert("welocme to page")
+// this is an alert mesaage 
+console.log("pranjal jain")
+// alert("this is my webpage")
+// var a=confirm("are u :")
+// if(a){
+//     console.log("u are ganda")
+// }
+// else{
+//     console.log("hat bc")
+// }
+
