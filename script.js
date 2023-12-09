f499da4655fb833706b190d0026fe10119a6e2a4
Pranjal Jain <pranjalworkon@gmail.com>
2023-12-09 13:44:51 +0530

    variable and datatype

--- script.js
+++ script.js
@@ -12,3 +12,89 @@ console.log("pranjal jain")
 //     console.log("hat bc")
 // }
 
+// variable in JS
+var a =5;
+a=a+5;
+var b=22;
+var c="pranjal"
+console.log(a);
+console.log(typeof(a));
+console.log(typeof(c));
+
+{
+    let a=3;
+    console.log(a)
+}
+
+// we will use the "let" for decalring thr variable as it can be updated 
+
+// moreover the type of the null is object and object in js is liek the key value pair in the { }brackets 
+// 2 types of date type ie primitive that is already predefined in js and other is object that is user dfined that is written in th eform if key value pair {} 
+
+let x="pranjal jain";
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;
+let o={
+    "name":"pranjal jain",
+    "job":"programmer",
+    "salary":10000000
+}
+// so basically it is an object that means we are using the user defined datatype and in the object we use the key value pair of the terms 
+
+console.log(x,y,z,p,q,r,o)
+console.log(typeof(x),typeof(y),typeof(z),typeof(p),typeof(q),typeof(r),typeof(o))
+
+// we can directly change the value of the object 
+o.salary=55
+console.log(o)
+
+// adding new key and the value of it 
+o.city="Delhi"
+console.log(o)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
