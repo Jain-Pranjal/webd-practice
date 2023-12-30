a7e51138bc60c440e7ffb786aabc4379361fff36
Pranjal Jain <pranjalworkon@gmail.com>
2023-12-30 22:44:57 +0530

    included the w3s files

---
+++ w3s.js
@@ -0,0 +1,45 @@
+// here i will be reading and implementing from the doc
+console.log(Date());
+// where do we need to link the js ?
+// at the above last of the body 
+
+// so we can add the evenet listener as 
+// document.getElementById("fds").addEventListener("any event that can takes place ",(e)=>{
+//     document.getElementById("fds").innerHTML=nfjsnd
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
