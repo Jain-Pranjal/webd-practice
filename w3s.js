643548e2b940bd9313f638c30f204d57622d9f4f
Pranjal Jain <pranjalworkon@gmail.com>
2023-12-31 21:33:50 +0530

    string methods

--- w3s.js
+++ w3s.js
@@ -9,11 +9,39 @@ console.log(Date());
 // })
 
 
+a=5
+console.log(a);
 
 
+// hoisting
+// Variables defined with var are hoisted to the top and can be initialized at any time.
+// Mean oisted 
+
+class animal{
+    "run"=true
+    name="sheru"
+}
+
+let hello=new animal()
 
 
-
+// String length
+// String slice()
+// String substring()
+// String substr()
+// String replace()
+// String replaceAll()
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String charAt()
+// String charCodeAt()
+// String split()
 
 
 
