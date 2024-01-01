133a24c4c7c98142bef4cf4cf1fc90562b5667ad
Pranjal Jain <pranjalworkon@gmail.com>
2024-01-01 20:53:51 +0530

    errors in js

--- w3s.js
+++ w3s.js
@@ -43,6 +43,16 @@ let hello=new animal()
 // String charCodeAt()
 // String split()
 
+// Errors
+/**Throw, and Try...Catch...Finally
+The try statement defines a code block to run (to try).
+
+The catch statement defines a code block to handle any error.
+
+The finally statement defines a code block to run regardless of the result.
+
+The throw statement defines a custom error. */
+
 
 
 
