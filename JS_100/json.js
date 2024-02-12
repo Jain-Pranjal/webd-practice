729275e81036f29d7d3f34881da2e8ee76171c2f
Pranjal Jain <pranjalworkon@gmail.com>
2024-02-12 19:17:34 +0530

    json intro

---
+++ JS_100/json.js
@@ -0,0 +1,30 @@
+// json is the lightweight file that is used to transefer the information across the web easily 
+// morover the json is wriiten in the string format only 
+// and json is in the key value pair 
+
+// nowdays 99% api returs the repsonse int he form fo json which is just similar to js object with the key and value pair 
+// so we can convert any json into js object and vice versa 
+
+JSON.stringify()
+// it converts the js object into the json 
+
+JSON.parse()
+// it convetts the json inot the object 
+
+// json is in the string fornat only which is in the "" 
+// also we need to use only the "" in making the json 
+
+
+// insode the json we can use any datatype in it but it should only be in the key value pair 
+
+// inside json anydataye can be used but the format of the json is only the string 
+// so we can pass the object array anything in the json in the key value pair only 
+
+// usign the parse it convetts into the js object 
+
+
+
+
+
+
+
